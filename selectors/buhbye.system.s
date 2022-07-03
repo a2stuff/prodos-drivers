145aba1f6683532e21a0e7d0723e118100d649fc
Joshua Bell <inexorabletash@gmail.com>
2022-07-03 15:29:04 -0700

    Buhbye.system: Clear keyboard strobe on launch
    This is particularly useful on MAME where you're likely to have just
    pressed Return to dismiss a warning.

--- selectors/buhbye.system.s
+++ selectors/buhbye.system.s
@@ -131,6 +131,8 @@ loop:   lda     (src_ptr)       ; *src_ptr = *dst_ptr
         cld                     ; ProDOS protocol for QUIT routine
         lda     ROMIN2          ; Page in ROM for reads, writes ignored
 
+        bit     KBDSTRB         ; Clear keyboard strobe
+
         lda     #$A0
         jsr     SLOT3           ; Activate 80-Column Firmware
 
